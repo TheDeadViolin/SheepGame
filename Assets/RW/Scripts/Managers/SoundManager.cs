ac7309e703a6a28c39674b2deb2343689764dbfe
Student <student@bilgi.stunetworks>
2022-04-04 16:53:00 +0300

    Sheep Gane

---
+++ Assets/RW/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    public AudioClip shootClip;
+    public AudioClip sheepDroppedClip;
+    public AudioClip sheepHitClip;
+    private Vector3 cameraPosition;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instance = this;
+        cameraPosition = Camera.main.transform.position;
+    }
+    private void PlaySound(AudioClip clip)
+    {
+        AudioSource.PlayClipAtPoint(clip, cameraPosition);
+    }
+    public void PlayShootClip()
+    {
+        PlaySound(shootClip);
+    }
+    public void PlaySheepHitClip()
+    {
+        PlaySound(sheepHitClip);
+    }
+    public void PlaySheepDroppedClip()
+    {
+        PlaySound(sheepDroppedClip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
